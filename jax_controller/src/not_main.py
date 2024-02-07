64d66c2b7f2967c536ac923ce2f33e6a7a55fdd4
Sebastian August Berg <sebasabe@stud.ntnu.no>
2024-02-07 15:18:36 +0100

    update

--- jax_controller/src/not_main.py
+++ jax_controller/src/not_main.py
@@ -1,10 +1,9 @@
 from controllers.ai_pid_controller import AIPIDController
 from plants.base_plant import BasePlant
 
-import config
+import config.config as config
 
 print(config.ACTIVATION_FUNCTION)
-
 
 
 print("done")
