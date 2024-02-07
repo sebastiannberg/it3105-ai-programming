c724df5a36dca95f9f50a01117cb16c3a8e34a80
Sebastian August Berg <sebasabe@stud.ntnu.no>
2024-02-07 15:21:51 +0100

    Update imports

--- jax_controller/src/main.py
+++ jax_controller/src/main.py
@@ -1,10 +1,3 @@
-# import os
-# import sys
-# current_dir = os.path.dirname(os.path.abspath(__file__))
-# parent_dir = os.path.dirname(current_dir)
-# sys.path.append(parent_dir) # To be able to import our own modules
-# print(sys.path)
-
 from math import sqrt
 
 import config.config as config
--- jax_controller/src/not_main.py
+++
@@ -1,9 +0,0 @@
-from controllers.ai_pid_controller import AIPIDController
-from plants.base_plant import BasePlant
-
-import config.config as config
-
-print(config.ACTIVATION_FUNCTION)
-
-
-print("done")
