b2bfefa3dc9883aa665516fc5f936673c4deaa58
Sebastian August Berg <sebasabe@stud.ntnu.no>
2024-02-07 13:45:57 +0100

    Possible import fix

--- jax_controller/src/main.py
+++ jax_controller/src/main.py
@@ -1,5 +1,9 @@
+import os
 import sys
-sys.path.append('jax_controller/') # To be able to import our own modules
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir) # To be able to import our own modules
+print(sys.path)
 
 from math import sqrt
 
