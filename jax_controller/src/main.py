cfecc25505d394797ee274a4df17449005abfd50
Sebastian August Berg <sebasabe@stud.ntnu.no>
2024-02-07 14:52:51 +0100

    try no path add

--- jax_controller/src/main.py
+++ jax_controller/src/main.py
@@ -1,9 +1,9 @@
-import os
-import sys
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir) # To be able to import our own modules
-print(sys.path)
+# import os
+# import sys
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# parent_dir = os.path.dirname(current_dir)
+# sys.path.append(parent_dir) # To be able to import our own modules
+# print(sys.path)
 
 from math import sqrt
 
