e70109d820df04423f3110e7460592bf87047612
Sebastian August Berg <sebasabe@stud.ntnu.no>
2024-03-22 17:45:37 +0100

    Add tests for classifier

--- deepstack_knock_off/tests/test_poker_oracle.py
+++ deepstack_knock_off/tests/test_poker_oracle.py
@@ -81,3 +81,112 @@ four_of_a_kind_tests = (
 for hand, public_cards in four_of_a_kind_tests:
     hand_type = oracle.classify_poker_hand(hand, public_cards)
     assert hand_type.category == "four_of_a_kind", f"Expected 'four_of_a_kind' but was '{hand_type.category}' in test with hand {[(card.rank, card.suit) for card in hand]} and public {[(card.rank, card.suit) for card in public_cards]}"
+
+full_house_tests = (
+    (
+        [Card("9", "hearts"), Card("9", "spades")],
+        [Card("7", "hearts"), Card("7", "diamonds"), Card("7", "clubs"), Card("2", "clubs"), Card("Q", "diamonds")]
+    ),
+    (
+        [Card("8", "hearts"), Card("2", "clubs")],
+        [Card("J", "hearts"), Card("8", "diamonds"), Card("5", "hearts"), Card("2", "hearts"), Card("2", "spades")]
+    ),
+)
+
+for hand, public_cards in full_house_tests:
+    hand_type = oracle.classify_poker_hand(hand, public_cards)
+    assert hand_type.category == "full_house", f"Expected 'full_house' but was '{hand_type.category}' in test with hand {[(card.rank, card.suit) for card in hand]} and public {[(card.rank, card.suit) for card in public_cards]}"
+
+flush_tests = (
+    (
+        [Card("J", "hearts"), Card("A", "spades")],
+        [Card("7", "hearts"), Card("3", "diamonds"), Card("5", "hearts"), Card("6", "hearts"), Card("Q", "hearts")]
+    ),
+    (
+        [Card("J", "hearts"), Card("J", "spades")],
+        [Card("7", "diamonds"), Card("3", "diamonds"), Card("5", "diamonds"), Card("6", "diamonds"), Card("Q", "diamonds")]
+    ),
+)
+
+for hand, public_cards in flush_tests:
+    hand_type = oracle.classify_poker_hand(hand, public_cards)
+    assert hand_type.category == "flush", f"Expected 'flush' but was '{hand_type.category}' in test with hand {[(card.rank, card.suit) for card in hand]} and public {[(card.rank, card.suit) for card in public_cards]}"
+
+straight_tests = (
+    (
+        [Card("J", "hearts"), Card("A", "spades")],
+        [Card("7", "hearts"), Card("8", "diamonds"), Card("9", "diamonds"), Card("10", "hearts"), Card("Q", "hearts")]
+    ),
+    (
+        [Card("A", "hearts"), Card("2", "spades")],
+        [Card("3", "spades"), Card("4", "diamonds"), Card("5", "diamonds"), Card("10", "spades"), Card("Q", "clubs")]
+    ),
+)
+
+for hand, public_cards in straight_tests:
+    hand_type = oracle.classify_poker_hand(hand, public_cards)
+    assert hand_type.category == "straight", f"Expected 'straight' but was '{hand_type.category}' in test with hand {[(card.rank, card.suit) for card in hand]} and public {[(card.rank, card.suit) for card in public_cards]}"
+
+three_of_a_kind_tests = (
+    (
+        [Card("J", "hearts"), Card("A", "spades")],
+        [Card("7", "hearts"), Card("J", "diamonds"), Card("9", "diamonds"), Card("J", "clubs"), Card("Q", "hearts")]
+    ),
+    (
+        [Card("A", "hearts"), Card("A", "spades")],
+        [Card("3", "spades"), Card("4", "diamonds"), Card("A", "diamonds"), Card("7", "spades"), Card("2", "clubs")]
+    ),
+    (
+        [Card("A", "hearts"), Card("2", "spades")],
+        [Card("3", "spades"), Card("3", "diamonds"), Card("3", "clubs"), Card("7", "spades"), Card("8", "clubs")]
+    ),
+)
+
+for hand, public_cards in three_of_a_kind_tests:
+    hand_type = oracle.classify_poker_hand(hand, public_cards)
+    assert hand_type.category == "three_of_a_kind", f"Expected 'three_of_a_kind' but was '{hand_type.category}' in test with hand {[(card.rank, card.suit) for card in hand]} and public {[(card.rank, card.suit) for card in public_cards]}"
+
+two_pair_tests = (
+    (
+        [Card("J", "hearts"), Card("A", "spades")],
+        [Card("7", "hearts"), Card("7", "diamonds"), Card("9", "diamonds"), Card("9", "clubs"), Card("Q", "hearts")]
+    ),
+    (
+        [Card("3", "hearts"), Card("6", "spades")],
+        [Card("3", "spades"), Card("4", "diamonds"), Card("A", "diamonds"), Card("7", "spades"), Card("7", "clubs")]
+    ),
+)
+
+for hand, public_cards in two_pair_tests:
+    hand_type = oracle.classify_poker_hand(hand, public_cards)
+    assert hand_type.category == "two_pair", f"Expected 'two_pair' but was '{hand_type.category}' in test with hand {[(card.rank, card.suit) for card in hand]} and public {[(card.rank, card.suit) for card in public_cards]}"
+
+pair_tests = (
+    (
+        [Card("J", "hearts"), Card("J", "spades")],
+        [Card("7", "hearts"), Card("2", "diamonds"), Card("9", "diamonds"), Card("A", "clubs"), Card("Q", "hearts")]
+    ),
+    (
+        [Card("3", "hearts"), Card("6", "spades")],
+        [Card("8", "spades"), Card("8", "diamonds"), Card("9", "diamonds"), Card("7", "spades"), Card("A", "clubs")]
+    ),
+)
+
+for hand, public_cards in pair_tests:
+    hand_type = oracle.classify_poker_hand(hand, public_cards)
+    assert hand_type.category == "pair", f"Expected 'pair' but was '{hand_type.category}' in test with hand {[(card.rank, card.suit) for card in hand]} and public {[(card.rank, card.suit) for card in public_cards]}"
+
+high_card_tests = (
+    (
+        [Card("J", "hearts"), Card("K", "spades")],
+        [Card("7", "hearts"), Card("2", "diamonds"), Card("9", "diamonds"), Card("A", "clubs"), Card("Q", "hearts")]
+    ),
+    (
+        [Card("3", "hearts"), Card("6", "spades")],
+        [Card("8", "spades"), Card("2", "diamonds"), Card("J", "diamonds"), Card("7", "spades"), Card("A", "clubs")]
+    ),
+)
+
+for hand, public_cards in high_card_tests:
+    hand_type = oracle.classify_poker_hand(hand, public_cards)
+    assert hand_type.category == "high_card", f"Expected 'high_card' but was '{hand_type.category}' in test with hand {[(card.rank, card.suit) for card in hand]} and public {[(card.rank, card.suit) for card in public_cards]}"
